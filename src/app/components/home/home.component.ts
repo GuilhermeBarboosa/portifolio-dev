21fdf001e7976d0551dbe0865de4c60c7107393e
GuilhermeBarboosa <guilherme.strg@gmail.com>
2024-10-01 15:28:50 -0300

    style pagina home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,10 @@
-import { Component, Pipe } from '@angular/core';
+import { Component, Inject, Pipe, PLATFORM_ID } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { CardLanguageComponent } from '../../shared/card-language/card-language.component';
+import Typewriter from 't-writer.js';
+import { isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,41 +13,19 @@ import { CardLanguageComponent } from '../../shared/card-language/card-language.
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  // fullText: string = 'Guilherme Barbosa'; // Texto completo
-  // displayedText: string = ''; // Texto que será exibido
-  // isDeleting: boolean = false; // Se está apagando ou escrevendo
-  // currentIndex: number = 0; // Índice atual da posição no texto
-  // speed: number = 150; // Velocidade de digitação em ms
-  // pause: number = 1000; // Pausa ao finalizar a ação
-  // interval: any; // Guarda o intervalo para limpar quando necessário
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-  // ngOnInit() {
-  //   this.startTyping(); // Inicia o efeito de digitação ao carregar o componente
-  // }
+  ngOnInit() {
+    // Verifica se o código está sendo executado no navegador
+    if (isPlatformBrowser(this.platformId)) {
+      const target = document.querySelector('.tw');
 
-  // startTyping() {
-  //   this.interval = setInterval(() => {
-  //     if (!this.isDeleting && this.currentIndex < this.fullText.length) {
-  //       this.currentIndex++;
-  //       this.displayedText = this.fullText.substring(0, this.currentIndex);
-  //     } else if (this.isDeleting && this.currentIndex > 1) {
-  //       this.currentIndex--;
-  //       this.displayedText = this.fullText.substring(0, this.currentIndex);
-  //     }
+      const writer = new Typewriter(target, {
+        loop: true,
+        typeColor: 'blue',
+      });
 
-  //     if (this.currentIndex === this.fullText.length) {
-  //       clearInterval(this.interval);
-  //       setTimeout(() => {
-  //         this.isDeleting = true;
-  //         this.startTyping();
-  //       }, this.pause);
-  //     } else if (this.currentIndex === 1 && this.isDeleting) {
-  //       clearInterval(this.interval);
-  //       setTimeout(() => {
-  //         this.isDeleting = false;
-  //         this.startTyping();
-  //       }, this.pause);
-  //     }
-  //   }, this.speed);
-  // }
+      writer.type('Guilherme Barbosa').rest(500).start();
+    }
+  }
 }
