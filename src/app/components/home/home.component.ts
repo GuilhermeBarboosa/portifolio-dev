21f05819647e285c8aad4d4f42be88441e13216f
GuilhermeBarboosa <guilherme.strg@gmail.com>
2024-10-02 09:35:46 -0300

    style grids

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { CardLanguageComponent } from '../../shared/card-language/card-language.component';
 import Typewriter from 't-writer.js';
 import { isPlatformBrowser } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -13,19 +14,52 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  public isSmallScreen = false;
+  private writer: any;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit() {
-    // Verifica se o código está sendo executado no navegador
     if (isPlatformBrowser(this.platformId)) {
-      const target = document.querySelector('.tw');
-
-      const writer = new Typewriter(target, {
-        loop: true,
-        typeColor: 'blue',
-      });
-
-      writer.type('Guilherme Barbosa').rest(500).start();
+      this.observerText();
     }
+  }
+
+  private writerType(text: string) {
+    let target = document.querySelector('.tw');
+
+    if (this.writer) {
+      target!.innerHTML = '';
+    }
+
+    this.writer = new Typewriter(target, {
+      loop: true,
+      typeColor: '#001aff',
+
+      cursorColor: 'white',
+      typeClass: 'type-span',
+      cursorClass: 'cursor-span',
+
+      typeSpeed: 'random',
+    });
+
+    this.writer.type(`${text}`).rest(500).start();
+  }
+
+  observerText() {
+    this.breakpointObserver
+      .observe(['(max-width: 800px)'])
+      .subscribe((res: any) => {
+        this.isSmallScreen = res.matches;
+
+        if (this.isSmallScreen) {
+          this.writerType('Guilherme');
+        } else {
+          this.writerType('Guilherme Barbosa');
+        }
+      });
   }
 }
